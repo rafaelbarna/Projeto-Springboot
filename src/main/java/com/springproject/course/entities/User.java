7205963a17a3c0bd7ed43865333cfe3f6d4dd8cd
rafaelbarna <rafael@barna.com.br>
2022-02-09 13:50:11 -0300

    User entity and resource

---
+++ src/main/java/com/springproject/course/entities/User.java
@@ -0,0 +1,89 @@
+package com.springproject.course.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	// Atributos
+	private Long id;
+	private String name;
+	private String email;
+	private String phone;
+	private String password;
+	
+	// Métodos
+	public User() {
+	}
+
+	public User(Long id, String name, String email, String phone, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	// Getters and Setters
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}

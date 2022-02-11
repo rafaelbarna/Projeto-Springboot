333394cae93d4986ee09615759fd1b7aa7c160e9
rafaelbarna <rafael@barna.com.br>
2022-02-11 16:15:30 -0300

    Product

--- src/main/java/com/springproject/course/entities/Product.java
+++ src/main/java/com/springproject/course/entities/Product.java
@@ -13,29 +13,35 @@ import javax.persistence.Table;
 import javax.persistence.Transient;
 
 @Entity
-@Table(name = "tb_category")
-public class Category implements Serializable {
+@Table(name = "tb_product")
+public class Product implements Serializable{
 	private static final long serialVersionUID = 1L;
 
-	// Atributos
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	// Atributos
 	private Long id;
 	private String name;
+	private String description;
+	private Double price;
+	private String imgUrl;
 	
 	@Transient
-	private Set<Product> products = new HashSet<>();
+	private Set<Category> categories = new HashSet<>();
 	
-	// Construtores
-	public Category() {
+	// Métodos
+	public Product() {
 	}
 
-	public Category(Long id, String name) {
+	public Product(Long id, String name, String description, Double price, String imgUrl) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.imgUrl = imgUrl;
 	}
-
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -49,15 +55,11 @@ public class Category implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		return Objects.equals(id, other.id);
 	}
 
 	// Getters and Setters
-	public Set<Product> getProducts() {
-		return products;
-	}
-	
 	public Long getId() {
 		return id;
 	}
@@ -66,11 +68,39 @@ public class Category implements Serializable {
 		return name;
 	}
 
+	public String getDescription() {
+		return description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public Set<Category> getCategories() {
+		return categories;
+	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
 	}
 }
